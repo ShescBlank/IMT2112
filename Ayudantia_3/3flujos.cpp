a0994348c249f8efa4e412b76db5e4690ee2805d
Alberto Almuna <alberto.almuna@uc.cl>
2022-08-24 11:14:55 -0400

    Update Ayudantía 2

--- Ayudantia_3/3flujos.cpp
+++ Ayudantia_3/3flujos.cpp
@@ -10,34 +10,105 @@ using namespace std;
 
 int main() {
 
-    // int a = 1; // Debemos declarar el tipo de variable
-    // int b; 
-    // b = 3;
-    // float c = 8;
-    // bool terminado = false;
-
-    // if (a+b > 2  && a%2 == 1) {
-    //     printf("a+b = %i\n", a+b); // Inyectar un int
-    // }
-    // else if (a+b == 2 || c == 8) { // Este de acá es un or
-    //     printf("c = %f, %f\n", c, c); // Inyectar dos floats
-    // }
-    // else {
-    //     // el else vacío no importa
-    // }
-
-    // for (int i=0; i<10; i++) { // el i++ es sumarle 1 al final de cada iteración
-    //     a += 1;
-    // }
-    // printf("a = %i\n", a);
+    int a, b, c;
+    a = 10;
+    b = 2;
+    c = 5;
 
 
-    // while (!terminado) { // not terminado
-    //     b += 1;
-    //     if (b % 4 == 0) {
-    //         terminado = true;
-    //     }
-    // }
+    // IF, ELSE IF, ELSE
+    printf("\nIF, ELSE IF, ELSE:\n");
+    if (a > c)
+    {
+        printf("a > c\n");
+    }
+    else if (a = c) {
+        printf("a = c\n");
+    }
+    else { printf("a < c\n"); }
+
+
+    // FOR
+    printf("\nFOR: i++\n");
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
+
+    printf("\nFOR: i*=2\n");
+    for (int i = 1; i < 10; i*=2)
+    {
+        cout << i << endl;
+    }
+
+    printf("\nFOR: continue\n");
+    for (int i = 0; i < 5; i++)
+    {
+       for (int j = 0; j < 5; j++)
+       {
+        if (i == j) 
+        {
+            continue;
+        }
+        printf("(%d, %d)\n", i, j);
+       }
+    }
+    
+
+    // WHILE
+    printf("\nWHILE:\n");
+    while (c < 8) 
+    {
+        cout << c++ << endl;
+    }
+    cout << c << endl;
+
+    printf("\nWHILE: true y break\n");
+    while (true) 
+    {
+        b -= 1;
+        if (b == -10)
+        {
+            printf("Rompemos el while\n");
+            break;
+        }
+        printf("Siguiente iteración...\n");
+    }
+
+    
+    // SWITCH STATEMENT
+    printf("\nSWITCH:\n");
+    int variable = 2;
+
+    switch (variable)
+    {
+    case 0:
+        cout << "Falso" << endl;
+        break;
+    
+    case 1:
+        cout << "Verdadero" << endl;
+        break;
+    
+    default:
+        cout << "Inválido" << endl;
+        break;
+    }
+
+    printf("\nSWITCH: sin break\n");
+    variable = 0;
+
+    switch (variable)
+    {
+    case 0:
+        cout << "Falso" << endl;
+    
+    case 1:
+        cout << "Verdadero" << endl;
+    
+    default:
+        cout << "Inválido" << endl;
+    }
     
     return 0;
 }
