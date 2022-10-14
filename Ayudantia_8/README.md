6db188220697bb2e032ae3b339ab5f84519aca93
Alberto Almuna <alberto.almuna@uc.cl>
2022-10-13 22:54:31 -0300

    Added Ayudantia 8

---
+++ Ayudantia_8/README.md
@@ -0,0 +1,7 @@
+Hola :D
+
+En esta ayudantía nos dedicamos a conversar sobre dudas de las tareas y a ver un poco de teoría sobre stencils.
+
+Los códigos corresponden a los que el profesor subió con la tarea. 
+
+Para la tarea, recomiendo echar un vistazo a las operaciones colectivas de MPI, como Allreduce, Reduce, Allgather y Allgatherv, estas pueden facilitar ciertas implementaciones.
---
+++ Ayudantia_8/tarea3_generate_matrix.py
@@ -0,0 +1,27 @@
+# Elwin van 't Wout
+# Pontificia Universidad Católica de Chile
+# 7-10-2022
+
+# Generate a matrix with real eigenvalues uniformly located in a specific
+# range and store the matrix in a text file.
+
+import numpy as np
+
+def generate_matrix(dim):
+    from scipy.stats import ortho_group
+    from scipy.sparse import spdiags
+    a = ortho_group.rvs(dim, random_state=0)
+    b = np.linspace(1., 10., dim)
+    return a @ spdiags(b, 0, dim, dim) @ a.T
+
+def write_matrix(matrix, filename='matrix.txt'):
+    with open(filename, 'w') as f:
+        f.write(str(mat.shape[0])+"\n"+str(mat.shape[1])+"\n")
+        for i in range(mat.shape[0]):
+            for j in range(mat.shape[1]):
+                f.write(str(mat[i,j])+"\n")
+        f.write("end")
+
+ndim = 2000
+mat = generate_matrix(ndim)
+write_matrix(mat)
